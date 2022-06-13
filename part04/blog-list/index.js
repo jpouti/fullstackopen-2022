18b796993fed8cad106862a03a2526fe9936e481
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-06-13 14:11:57 +0300

    refactor application to models/blog.js & utils/config.js & utils/logger.js

--- part04/blog-list/index.js
+++ part04/blog-list/index.js
@@ -1,40 +1,10 @@
-//const logger = require('./utils/logger');
-require('dotenv').config();
-const express = require('express')
-const cors = require('cors')
-const mongoose = require('mongoose')
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const express = require('express');
+const cors = require('cors');
+const Blog = require('./models/blog');
 
 const app = express()
-
-// ---- MONGOOSE
-
-const mongoUrl = process.env.MONGO_URL;
-mongoose.connect(mongoUrl)
-    .then(() => {
-        console.log('Connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB', error)
-    })
-
-const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
-
-blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
-  
-const Blog = mongoose.model('Blog', blogSchema);
-
-
 
 app.use(cors())
 app.use(express.json())
@@ -58,7 +28,6 @@ app.post('/api/blogs', (request, response) => {
     .catch(error => next(error))
 })
 
-const PORT = process.env.PORT;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
---
+++ part04/blog-list/models/blog.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+const config = require('../utils/config');
+const logger = require('../utils/logger');
+
+mongoose.connect(config.MONGO_URL)
+    .then(() => {
+        logger.info('Connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB', error)
+    })
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number
+})
+
+blogSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+  })
+  
+module.exports = mongoose.model('Blog', blogSchema);
