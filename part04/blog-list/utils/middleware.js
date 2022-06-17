bf5d08537416d36005dd7756c7dd99e116a08214
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-06-17 17:29:14 +0300

    Fixed a typo / bug middleware.errorHandler

--- part04/blog-list/utils/middleware.js
+++ part04/blog-list/utils/middleware.js
@@ -17,7 +17,7 @@ const unknownEndpoint = (request, response) => {
 const errorHandler = (error, request, response, next) => {
   logger.error(error.message)
   if(error.name === 'ValidationError') {
-    return response.status(400).jso({ error: error.message })
+    return response.status(400).json({ error: error.message })
   } else if(error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   } else if (error.name === 'TokenExpiredError') {
