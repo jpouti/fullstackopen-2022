2d956fad396f41a9dedf80b0cb78e44528d015aa
Juho Poutiainen <j.poutiainen94@gmail.com>
2022-07-15 02:06:40 +0300

    Add solution for exercises 9.1 - 9.3

---
+++ part09/typescript-first-steps/bmiCalculator.ts
@@ -0,0 +1,52 @@
+interface BmiValues {
+    value1: number;
+    value2: number;
+}
+
+const parseArguments = (args: Array<string>): BmiValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if(!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3]),
+        }
+    } else {
+        throw new Error('Provided values were not numbers')
+    }
+
+}
+// height as cm & mass as kg
+const calculateBmi = (height: number, mass: number) => {
+    const heightToM2 = (0.01 * height) ** 2;
+    const bmi = mass / heightToM2;
+    if (bmi < 16) {
+        console.log('Underweight (Severe thinnes)');
+    } else if (bmi >= 16 && bmi < 16.9) {
+        console.log('Underweight (Moderate thinness)');
+    } else if (bmi >= 17 && bmi < 18.4) {
+        console.log('Underweight (Mild thinness)');
+    } else if (bmi >= 18.5 && bmi < 24.9) {
+        console.log('Normal (healthy weight)');
+    } else if (bmi >= 25 && bmi < 29.9) {
+        console.log('Overweight (Pre-obese)');
+    } else if (bmi >= 30 && bmi < 34.9) {
+        console.log('Obese (Class I)');
+    } else if (bmi >= 35 && bmi < 39.9) {
+        console.log('Obese (Class II)');
+    } else if (bmi >= 40) {
+        console.log('Obese (Class III)')
+    }
+}
+
+try {
+    const { value1, value2 } = parseArguments(process.argv);
+    calculateBmi(value1, value2);
+} catch (error:unknown) {
+    let errorMessage = 'Something bad happened';
+    if (error instanceof Error) {
+        errorMessage += ' Error ' + error.message
+    }
+    console.log(errorMessage)
+}
---
+++ part09/typescript-first-steps/exerciseCalculator.ts
@@ -0,0 +1,77 @@
+interface Result {
+    periodLength: number,
+    traininDays: number,
+    success: boolean,
+    rating: number,
+    ratingDescription: string,
+    target: number,
+    average: number
+}
+
+let exerciseHours: number[] = [];
+let target;
+
+const parseArgumentsCalculator = (args: Array<string>) => {
+    if (args.length < 4) throw new Error('not enough arguments');
+    target = Number(args[2])
+    for (let index = 3; index < args.length; index++) {
+        if (!isNaN(Number(args[index]))) {
+            exerciseHours.push(Number(args[index]))
+        } else {
+            throw new Error('provided values were not numbers')
+        }
+    }
+    console.log(target, 'target')
+    console.log(exerciseHours, 'hours')
+    return {
+        target,
+        exerciseHours
+    }
+}
+
+const calculateExercises = (target: number, exerciseHours: Array<number>): Result => {
+    const periodLength = exerciseHours.length;
+
+    const traininDays = exerciseHours.length - exerciseHours.filter(e => e === 0).length;
+
+    const average = exerciseHours.reduce((a, b) => a + b) / exerciseHours.length;
+
+    let rating;
+    let success;
+    let ratingDescription;
+
+    if (target < average) {
+        success = true;
+        rating = 3;
+        ratingDescription = 'You have done great, keep up with the same way!'
+    } else if ((target / 2) < average && target > average ) {
+        success = false;
+        rating = 2;
+        ratingDescription = 'not too bad but you could do better...'
+    } else if ((target / 2) > average) {
+        success = false;
+        rating = 1;
+        ratingDescription = 'You can improve for the next training period'
+    }
+
+    return {
+        periodLength,
+        traininDays,
+        success,
+        rating,
+        ratingDescription,
+        target,
+        average
+    }
+}
+
+try {
+    const { target, exerciseHours } = parseArgumentsCalculator(process.argv);
+    console.log(calculateExercises(target, exerciseHours))
+} catch (error:unknown) {
+    let errorMessage = 'Something bad happened';
+    if (error instanceof Error) {
+        errorMessage += ' Error ' + error.message
+    }
+    console.log(errorMessage)
+}
